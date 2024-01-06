9ec1cb3865824f1f1493cb28171bc7710baebab0
mhoppitt <mhoppitt18@outlook.com>
2024-01-06 18:02:21 +1100

    implement basic next.js configuration

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import '@mantine/core/styles.css';
+import React from 'react';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
+import { theme } from '../theme';
+
+export const metadata = {
+  title: 'Matt Hoppitt | Developer',
+};
+
+export default function RootLayout({ children }: { children: any }) {
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript />
+        <link rel="shortcut icon" href="/logo.png" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
+      </body>
+    </html>
+  );
+}
