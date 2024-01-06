e561da65d9c93f32916537cf23cdda513293ccaa
mhoppitt <mhoppitt18@outlook.com>
2024-01-06 21:16:04 +1100

    responsive formatting for home page

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,25 +1,27 @@
-import { Title, Text, Anchor, Image, Center, Button } from '@mantine/core';
+import { Title, Text, Anchor, Image, Flex, Space } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
     <>
-      <Title className={classes.title} ta="center" mt={100}>
+      <Title className={classes.title} ta="center" mt={75}>
         Welcome to{' '}
         <Text inherit c="mainBlue.7" component="span">
           matthoppitt.me
         </Text>
       </Title>
-      <Text c="dimmed" ta="center" size="lg" maw={1000} mx="auto" mt="xl">
+      <Text c="dimmed" ta="center" size="lg" mx="auto" mt="xl">
         Stay tuned for a comprehensive breakdown of all my personal projects. Please feel free to connect with me on{' '}
         <Anchor href="https://au.linkedin.com/in/matt-hoppitt" target="_blank" size="lg">
           LinkedIn
         </Anchor>
         .
       </Text>
-      <Center mt="xl">
+      
+      <Space h="xl" />
+
+      <Flex wrap="wrap" gap="xl" align="center" justify="center">
         <Image
-          px={30}
           radius="md"
           h={200}
           w="auto"
@@ -27,14 +29,13 @@ export function Welcome() {
           src="/logo.png"
         />
         <Image
-          px={30}
           radius="md"
           h={200}
           w="auto"
           fit="contain"
           src="/avatar.png"
         />
-      </Center>
+      </Flex>
     </>
   );
 }
